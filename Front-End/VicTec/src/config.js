d5075bedb6b2e06c4bba9509b3442ae570332547
VicenteAndres03 <vicepachecoa@gmail.com>
2025-11-22 19:06:28 -0300

    ngrok puesto

--- Front-End/VicTec/src/config.js
+++ Front-End/VicTec/src/config.js
@@ -1,4 +1,4 @@
 // src/config.js
-const API_URL = 'https://victec.onrender.com/api/v1'; // Tu URL de Render
+const API_URL = "https://nonciliate-elois-crapulously.ngrok-free.app/api/v1";
 
 export default API_URL;
